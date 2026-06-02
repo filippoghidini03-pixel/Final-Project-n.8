8c9bdd610c6ef6951253d50b48ff70dc116867cd
filippoghidini03-pixel <filippoghidini03@gmail.com>
2026-06-02 11:33:48 +0200

    Changes

--- Run_project_8.m
+++ Run_project_8.m
@@ -1,4 +1,5 @@
 %% Run_project_8.m
+tic
 clear all; 
 close all; 
 clc;
@@ -83,3 +84,4 @@ save('Part_E.mat', 'FSI_euro_z', 'FSI_italy_z', 'FSI_spain_z');
 fprintf('=== Part E Complete ===\n');
 %% Comparison ASW vs Zeta spread
 plotASWvsZeta(dates_BTP, spread10y_BTP, dates_BTP_z, spread10y_BTP_z, dates_BON, spread10y_BON, dates_BON_z, spread10y_BON_z);
+tempo=toc
